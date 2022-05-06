0839d049c7a40c6fb922132edfd6513f2b618993
moonelysian <dandyblack@naver.com>
2022-05-06 12:05:23 +0900

    add: autoplay 기능 추가

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,17 +1,18 @@
 export default class Swiper {
   #currentPosition = 0;
-
   #slideNumber = 0;
-
   #slideWidth = 0;
+  #autoPlay = true;
+  #intervalId;
 
   swiperWrapEl;
-
   swiperListEl;
-
   prevBtnEl;
+  nextBtnEl;
 
   indicatorWrapEl;
+
+  controlWrapEl;
 
   constructor() {
     this.assignElement();
@@ -21,6 +22,7 @@ export default class Swiper {
     this.addEvent();
     this.createIndicator();
     this.setIndicator();
+    this.initAutoPlay();
   }
 
   assignElement() {
@@ -29,6 +31,9 @@ export default class Swiper {
     this.nextBtnEl = this.swiperWrapEl.querySelector('#next');
     this.prevBtnEl = this.swiperWrapEl.querySelector('#previous');
     this.indicatorWrapEl = this.swiperWrapEl.querySelector('#indicator-wrap');
+    this.controlWrapEl = this.swiperWrapEl.querySelector('#control-wrap');
+    this.pauseBtnEl = this.controlWrapEl.querySelector('#pause');
+    this.playBtnEl = this.controlWrapEl.querySelector('#play');
   }
 
   initSlideNumber() {
@@ -43,6 +48,10 @@ export default class Swiper {
     this.swiperListEl.style.width = `${this.#slideNumber * this.#slideWidth}px`;
   }
 
+  initAutoPlay() {
+    this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+  }
+
   addEvent() {
     this.nextBtnEl.addEventListener('click', this.moveToRight.bind(this));
     this.prevBtnEl.addEventListener('click', this.moveToLeft.bind(this));
@@ -50,9 +59,24 @@ export default class Swiper {
       'click',
       this.onClickIndicator.bind(this),
     );
+    this.controlWrapEl.addEventListener('click', this.togglePlay.bind(this));
   }
 
-  onClickIndicator() {
+  togglePlay(event) {
+    if (event.target.dataset.status === 'play') {
+      this.#autoPlay = true;
+      this.controlWrapEl.classList.add('play');
+      this.controlWrapEl.classList.remove('pause');
+      this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+    } else if (event.target.dataset.status === 'pause') {
+      this.#autoPlay = false;
+      this.controlWrapEl.classList.add('pause');
+      this.controlWrapEl.classList.remove('play');
+      clearInterval(this.#intervalId);
+    }
+  }
+
+  onClickIndicator(event) {
     const indexPosition = parseInt(event.target.dataset.index, 10);
     if (Number.isInteger(indexPosition)) {
       this.#currentPosition = indexPosition;
@@ -71,6 +95,11 @@ export default class Swiper {
     this.swiperListEl.style.left = `-${
       this.#slideWidth * this.#currentPosition
     }px`;
+
+    if (this.#autoPlay) {
+      clearInterval(this.#intervalId);
+      this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+    }
     this.setIndicator();
   }
 
@@ -82,6 +111,11 @@ export default class Swiper {
     this.swiperListEl.style.left = `-${
       this.#slideWidth * this.#currentPosition
     }px`;
+
+    if (this.#autoPlay) {
+      clearInterval(this.#intervalId);
+      this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+    }
     this.setIndicator();
   }
 
